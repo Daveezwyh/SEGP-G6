b34106f9f8e7e6969cc835805c219803b983de8e
Daveez <>
2024-10-24 16:07:13 +0800

    JWT

--- backend/autoclean/api/views.py
+++ backend/autoclean/api/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.exclude(is_superuser=True)
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
