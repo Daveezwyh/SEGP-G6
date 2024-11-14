860828abf92b059218e556cef2cb7addd9a5d888
Daveez <>
2024-11-14 14:51:14 +0800

    Frontend Base Version

--- frontend/autoclean/src/api/index.jsx
+++ frontend/autoclean/src/api/index.jsx
@@ -1,6 +1,3 @@
-//api
-export const api_token = "http://35.213.150.144:8000/api/token";
-export const api_user = "http://35.213.150.144:8000/api/user";
-export const api_posts = "http://35.213.150.144:8000/api/posts";
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-export const base_url = "http://35.213.150.144:8000/api";
+export const api_token = `${baseUrl}/api/token`;
