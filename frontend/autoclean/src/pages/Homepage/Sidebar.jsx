4aa7fc505d21b64385b4f40fdb7058f81ca7d762
XiaWenjun <hcywx1@nottingham.edu.my>
2025-02-12 18:04:31 +0800

    Fix token localstorage undefined error. Finish log out function(Delete token).

--- frontend/autoclean/src/pages/Homepage/Sidebar.jsx
+++ frontend/autoclean/src/pages/Homepage/Sidebar.jsx
@@ -4,10 +4,12 @@ import { IoTimeOutline } from "react-icons/io5";
 import { IoIosLogOut } from "react-icons/io";
 import { GoSidebarExpand, GoSidebarCollapse } from "react-icons/go";
 import { HiUserCircle } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
     const [isOpen, setIsOpen] = useState(false);
     const[showLogout, setShowLogout] = useState(false);
+    const navigate = useNavigate();
 
     const toggleSidebar = () => {
         setIsOpen(prevState => !prevState);
@@ -21,6 +23,11 @@ export default function Sidebar() {
         setShowLogout(false);
     }
 
+    const handleLogout = () => {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        navigate("/");
+    };
 
     return (
         <>
@@ -53,8 +60,8 @@ export default function Sidebar() {
                             <button onClick={closeLogout} className="btn btn-secondary px-4">
                                 Cancel
                             </button>
-                            <button className="btn btn-danger px-4">
-                                Log Out
+                            <button onClick={handleLogout} className="btn btn-danger px-4">
+                                    Log Out
                             </button>
                         </div>
                     </div>
