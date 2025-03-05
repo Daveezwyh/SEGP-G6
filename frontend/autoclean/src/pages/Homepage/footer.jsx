2c8addbb9a85d7d6e479fb9c9ab86fe902e4cfc4
XiaWenjun <hcywx1@nottingham.edu.my>
2025-03-05 10:10:08 +0800

    Fix bug

--- frontend/autoclean/src/pages/Homepage/footer.jsx
+++ frontend/autoclean/src/pages/Homepage/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <footer className="fixed left-0 bottom-0 w-full dark:text-cyan-400">
-            © 2024 Your Company. All rights reserved.
+            © 2024 AutoClean. All rights reserved.
         </footer>
     );
 }
