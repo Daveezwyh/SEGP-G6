6b9aafd4835cdc4a6e04307c18cfadbefc92b081
Daveez <>
2025-03-17 11:21:13 +0800

    Working on scanners...

--- backend/autoclean/autoclean/scanners/manager.py
+++ backend/autoclean/autoclean/scanners/manager.py
@@ -5,7 +5,8 @@ class ScannerManager:
         self.scanners = [
             scan_df_for_duplicates,
             scan_df_for_missing,
-            scan_df_for_outliers
+            scan_df_for_outliers,
+            scan_df_for_categorical,
         ]
     
     def get_scanners(self):
